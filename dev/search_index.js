108f5ab918944ae280c2628a57093f3c2773c81f
Documenter.jl <documenter@juliadocs.github.io>
2020-12-05 23:17:02 +0000

    build based on 09253e8

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ZulipFinBot","category":"page"},{"location":"#ZulipFinBot","page":"Home","title":"ZulipFinBot","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ZulipFinBot]","category":"page"}]
+}
